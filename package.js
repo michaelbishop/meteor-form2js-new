0a0160a7d5084104172a01f07af67323b5f639e7
Michael Bishop <michael@michaelsplace.net>
2013-07-09 13:25:37 -0700

    initial commit with fixes

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+    summary: "Javascript library for collecting form data with jquery wrapper"
+});
+
+Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.add_files([
+        "lib/form2js/src/form2js.js",
+        "lib/form2js/src/js2form.js",
+        "lib/form2js/src/jquery.toObject.js",
+    ], "client");
+});
